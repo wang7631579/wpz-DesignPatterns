6eac5ee13905e6cf5b1681eaa0c1145154300482
wang7631579 <wangjun7631579@163.com>
2020-04-21 21:11:26 +0800

    设计模式第一版

---
+++ wpz-DesignPatterns/src/com/wpz/T.java
@@ -0,0 +1,26 @@
+package com.wpz;
+
+
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class T {
+
+	public static void main(String[] args) {
+		
+		Frame f = new Frame();
+		f.setSize(800,600);
+		f.setResizable(false);
+		f.setTitle("WPZ");
+		f.setVisible(true);
+		f.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+
+}
